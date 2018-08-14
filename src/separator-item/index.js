a21bcc99818e143eacc3973d2f4941814c722e75
xiangyi <yi.xiang@live.cn>
2018-08-14 15:04:22 +0800

    0.0.5

--- src/separator-item/index.js
+++
@@ -1,8 +0,0 @@
-import SeparatorItem from '../separator/src/separator-item'
-
-/* istanbul ignore next */
-SeparatorItem.install = function (Vue) {
-  Vue.component(SeparatorItem.name, SeparatorItem)
-}
-
-export default SeparatorItem
--- src/separator/index.js
+++
@@ -1,8 +0,0 @@
-import Separator from './src/separator'
-
-/* istanbul ignore next */
-Separator.install = function (Vue) {
-  Vue.component(Separator.name, Separator)
-}
-
-export default Separator
